d0515c575ce08098ae16ecd611270ce26339541f
Şafak Can Baş <can.bas.cs@gmail.com>
2023-09-04 15:54:58 +0300

    Initial Commit

---
+++ BragiMovie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BragiMovie
+//
+//  Created by Can Bas on 4.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
